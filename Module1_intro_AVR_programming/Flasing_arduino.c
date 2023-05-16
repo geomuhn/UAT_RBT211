27b20a5c1afc93d69b2d89eed8c9babd35851e16
geomuhn <44282724+geomuhn@users.noreply.github.com>
2023-05-16 13:48:08 -0700

    Create Flasing_arduino.c

---
+++ Module1_intro_AVR_programming/Flasing_arduino.c
@@ -0,0 +1,23 @@
+/*
+ * Flasing_arduino.c
+ *
+ * Created: 5/16/2023 1:23:43 PM
+ * Author : George Muhn
+ */ 
+
+#ifdef F_CPU
+#define F_CPU 160000UL
+#endif
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRB |= (1<<PB5); //Sets only pin 13 as an output = 00100000
+    /* Replace with your application code */
+    while (1) {
+		PORTB |= (1<<PB5);
+    }
+}
+
